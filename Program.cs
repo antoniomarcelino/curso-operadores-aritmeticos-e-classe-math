9e52663c1ff1bb8c39e9775e4416139b1812652b
antoniomarcelino <antoniomarcelinodev@gmail.com>
2022-12-09 12:37:46 -0300

    criando-nossa-classe-calculadora

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using curso_operadores_aritmeticos_e_classe_math.bin;
+
+Calculadora calc = new Calculadora();
+
+calc.Somar(10, 30);
+calc.Subtrair(10, 50);
+calc.Multiplicar(15, 45);
+calc.Dividir(2, 2);
+
