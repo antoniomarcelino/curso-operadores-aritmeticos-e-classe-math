7b6d3eaee37a9dc45629bb62317262cfa6489d0d
antoniomarcelino <antoniomarcelinodev@gmail.com>
2022-12-09 12:54:56 -0300

    incremento-decremento

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using curso_operadores_aritmeticos_e_classe_math.bin;
+﻿/*using curso_operadores_aritmeticos_e_classe_math.bin;
 
 Calculadora calc = new Calculadora();
 
@@ -10,4 +10,19 @@ calc.Potencia(4, 2);
 calc.Seno(30);
 calc.Cosseno(30);
 calc.Tangente(30);
+*/
+
+int numeroIncremento = 10;
+Console.WriteLine(numeroIncremento);
+Console.WriteLine("Incrementando o 10");
+// numeroIncremento = numeroIncremento + 1;
+numeroIncremento++;
+Console.WriteLine(numeroIncremento);
+
+int numeroDecremento = 21;
+Console.WriteLine(numeroDecremento);
+Console.WriteLine("Decrementando o 21");
+// numeroDecremento = numeroDecremento - 1;
+numeroDecremento--;
+Console.WriteLine(numeroDecremento);
 
